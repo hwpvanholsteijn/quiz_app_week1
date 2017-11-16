2747277a754eb661d38c3531f85781ab7724a135
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2017-11-16 19:05:43 +0100

    added open-questions

---
+++ quiz_opdracht_weekEen/quiz_opdracht_weekEen/Models/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quiz_opdracht_weekEen.Models
+{
+    class Question
+    {
+        private byte difficulty;
+
+        public string Text { get; set; }
+        public string Awnser { get; set; }
+        public byte Difficulty
+        {
+            get { return difficulty; }
+            set {
+                switch (value) {
+                    case 3: difficulty = 3; break;
+                    case 2: difficulty = 2; break;
+                    default: difficulty = 1; break;
+                }
+            }
+        }
+        public string Categorie { get; set; }
+    }
+}
---
+++ quiz_opdracht_weekEen/quiz_opdracht_weekEen/Program.cs
@@ -0,0 +1,20 @@
+﻿using quiz_opdracht_weekEen.Models;
+using System;
+
+namespace quiz_opdracht_weekEen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question q = new Question();
+            q.Difficulty = 4;
+
+            Console.WriteLine(q.Difficulty);
+            q.Difficulty = 3;
+            Console.WriteLine(q.Difficulty);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
