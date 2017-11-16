2f747d1912a8cfcbd57a1cceada85596c64a8853
Huub van Holsteijn <hwpvanholsteijn@gmail.com>
2017-11-16 20:40:35 +0100

    Een aantal leeslijnen toegevoegd voor het leesbaar maken van de quiz

--- quiz_opdracht_weekEen/quiz_opdracht_weekEen/Models/QuizMaster.cs
+++ quiz_opdracht_weekEen/quiz_opdracht_weekEen/Models/QuizMaster.cs
@@ -13,20 +13,23 @@ namespace quiz_opdracht_weekEen.Models
 
 
         private void FillQuestions() {
+            Console.WriteLine("Random quiz");
             unAskedQuestions = qMan.RandomQuestions();
         }
 
         public void PresentQuestion()
         {
                 this.FillQuestions();
+                Console.WriteLine("----------------------------------------------------------------------------------------");
                 this.PresentQuestion(unAskedQuestions.Dequeue());
         }
 
         public void PresentQuestion(Question q) {
-            Console.WriteLine(q.Text+"\n");
+            Console.WriteLine(q.Text+"");
             Console.Write("Jou antwoord:  ");
             String inputLine = Console.ReadLine();
             Console.WriteLine(string.Format("Het antwoord dat je hebt gegeven is: {0}", q.IsCorrect(inputLine)));
+            Console.WriteLine("----------------------------------------------------------------------------------------");
             if (unAskedQuestions.Count > 0)
             {
                 this.PresentQuestion(unAskedQuestions.Dequeue());
